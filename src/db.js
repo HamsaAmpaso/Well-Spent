654dab4077c636039636e792ee6b865e099e8c0f
Hamsa Ampaso <hamsaampaso1@gmail.com>
2026-08-14 09:30:17 -1100

    changed database connection configuration

--- src/db.js
+++ src/db.js
@@ -3,11 +3,8 @@ dotenv.config();
 import pg from 'pg';
 const {Pool} = pg;
 export const pool = new Pool({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
+        
+        connectionString: process.env.DATABASE_URL
 
 });
 
