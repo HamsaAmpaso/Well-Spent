8a6293ad5b04ce0f551906ea731831e5b5b16b08
Hamsa Ampaso <hamsaampaso1@gmail.com>
2026-08-15 19:31:53 -1100

    fixed ui for archives expenses

--- src/db.js
+++ src/db.js
@@ -3,8 +3,12 @@ dotenv.config();
 import pg from 'pg';
 const {Pool} = pg;
 export const pool = new Pool({
-        
-        connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
+   /* host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE*/
 
 });
 
